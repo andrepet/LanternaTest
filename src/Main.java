84b95084853847abb9e6151eb46ac77eda26a917
andrepet <pettersson.andreas.lennart@gmail.com>
2018-02-27 16:30:32 +0100

    haaaaaands

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.nio.charset.Charset;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Terminal terminal = TerminalFacade.createTerminal(System.in, System.out, Charset.forName("UTF8"));
+        terminal.enterPrivateMode();
+    }
+}
